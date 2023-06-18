dee5bc99dda6b77ec2800c3b0025d49e79458207
dk9mbs <dk9mbs@gmail.com>
2023-06-18 22:07:25 +0200

    init

---
+++ plugins/escm_plugin_import_delvry.py
@@ -0,0 +1,8 @@
+from core.fetchxmlparser import FetchXmlParser
+from services.database import DatabaseServices
+from core import log
+
+logger=log.create_logger(__name__)
+
+def execute(context, plugin_context, params):
+    pass
